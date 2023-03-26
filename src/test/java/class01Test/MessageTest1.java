0c3e73ac38cec0d9b4f1b58504d3c1e817afeac0
Димитрија Тимески <32930367+DimitrijaT@users.noreply.github.com>
2023-03-26 23:30:57 +0200

    Unit Testing 1 & 2

---
+++ src/test/java/class01Test/MessageTest1.java
@@ -0,0 +1,36 @@
+package class01Test;
+
+import skit.class01.Message;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class MessageTest1 {
+
+    String message = "Tom";
+    Message messageObject = new Message(message);
+
+    @Test
+    public void testPrintMessageTrue() {
+        assertEquals(message, messageObject.printMessage());
+    }
+
+    @Test
+    public void testPrintMessageFalse() {
+        assertNotEquals("Mark", messageObject.printMessage());
+    }
+
+    @Test
+    public void testPrintMessageWithDuration() {
+        assertTimeout(Duration.ofMillis(5000), () ->
+        {
+            messageObject.printMessage();
+
+            // Thread.sleep(1000)
+        });
+    }
+
+}
